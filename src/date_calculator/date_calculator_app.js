7433935e044205cdab2d4acf43c95a2244e9333e
MC Squared <j.milesy.nz@gmail.com>
2018-02-02 11:31:42 +1300

    Basic calendar logic working

---
+++ src/date_calculator/date_calculator_app.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import logo from '.././logo.svg';
+import Calendar from 'react-calendar'
+
+function inDays(dd) {
+  return Math.ceil(dd/(24*3600*1000));
+}
+
+function inWeeks(dd) {
+  return Math.floor(inDays(dd)/7);
+}
+
+function Display(props) {// extends Component {
+    const dt = props.endDate.getTime() - props.startDate.getTime();
+
+    return (
+      <div className='display-inner'>
+        <p><b>Days: </b>{inDays(dt)}</p>
+        <p><b>Weeks: </b>{inWeeks(dt)}</p>
+      </div>
+    );
+}
+
+class DateCalculatorApp extends Component {
+  constructor(props) {
+    super(props);
+
+    var st = new Date();
+    st.setHours(0, 0, 0, 0)
+    this.state = {
+      start_date: st,
+      end_date: st,
+    }
+  }
+
+  //onChangeStart = date => this.setState({ date })
+  onChange(start, end) {
+    start = start || this.state.start_date
+    end = end || this.state.end_date
+
+    this.setState({
+      start_date: start,
+      end_date: end,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="content">
+          <div className="calendar">
+            <h2>Start</h2>
+            <Calendar
+              onChange={(date) => this.onChange(date, null)}// this.onChangeStart}
+              value={this.state.start_date}
+            />
+          </div>
+          <div className="calendar">
+            <h2>End</h2>
+            <Calendar
+              onChange={(date) => this.onChange(null, date)}// this.onChangeStart}
+              value={this.state.end_date}
+            />
+          </div>
+          <div className="display">
+            <Display
+              endDate={this.state.end_date}
+              startDate={this.state.start_date}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DateCalculatorApp;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import DateCalculatorApp from './date_calculator/date_calculator_app';
+
+console.log(window.location.hostname);
+switch(window.location.hostname) {
+  case 'localhost':
+    ReactDOM.render(<DateCalculatorApp />, document.getElementById('root'));
+    break;
+  default:
+    ReactDOM.render(<App />, document.getElementById('root'));
+    break;
+}
+
 registerServiceWorker();

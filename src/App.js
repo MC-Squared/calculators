38bcf98307dc640f0e23ac6c954b74ab60d5e49f
J Miles <j.milesy.nz@gmail.com>
2018-02-04 10:54:42 +1300

    Move routing from index to App

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import DateCalculatorApp from './date_calculator/date_calculator_app';
+import NavBar from './NavBar'
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    switch(window.location.hostname) {
+      case 'localhost':
+        return (
+          <div>
+            <NavBar />
+            <DateCalculatorApp />
+          </div>
+        );
+      default:
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        );
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-import DateCalculatorApp from './date_calculator/date_calculator_app';
-
-console.log(window.location.hostname);
-switch(window.location.hostname) {
-  case 'localhost':
-    ReactDOM.render(<DateCalculatorApp />, document.getElementById('root'));
-    break;
-  default:
-    ReactDOM.render(<App />, document.getElementById('root'));
-    break;
-}
+  
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
